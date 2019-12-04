ca69137fdcbbb6013f8d542cd50b00721df4fce6
Luke Sheedy <thesheedy@hotmail.com>
2019-12-03 17:47:44 -0800

    Running Code
    Finished step 34

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,11 +4,8 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.WriteLine("Remember, we’re “accounting” on you!");
+        Console.WriteLine("Hello, Wrold!");
         Console.ReadLine();
-
-
     }
 }
 
