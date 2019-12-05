721631b5c1458e8b995019e012d76380dd451eb3
Luke Sheedy <thesheedy@hotmail.com>
2019-12-05 14:54:55 -0800

    Finishing Step 66
    Finished all 5 exercises !

---
+++ Exercise 66/Exercise 66/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////1:multiplies by 50 
+            //Console.WriteLine("Provide a number  to multiply by 50 :");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //int mult = userInput * 50;
+            //Console.WriteLine("Reslut is " + mult);
+            //Console.ReadLine();
+
+            ////2: takes input of user adds 25
+            //Console.WriteLine("Provide a number so we can add 25 :");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //int add = userInput + 25;
+            //Console.WriteLine("Reslut is " + add);
+            //Console.ReadLine();
+
+            ////3: takes inputer and devies by 12.5
+            //Console.WriteLine("Provide a number so we can divide by 12.5.");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //decimal divid = 12.5m;
+            //decimal total = userInput / divid;
+            //Console.WriteLine("Your total is : "+total);
+            //Console.ReadLine();
+
+            //4: takes input and checks if greater than 50 and prints true or false
+            //Console.WriteLine("Please provide a number :");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(userInput > 50); // WILL return True  if userInput is higher than 50 
+            //Console.ReadLine();
+
+            //5: takes input from user divides by 7 and returns remiander
+            //Console.WriteLine("Provide a number so we can divide by 7 and see the remainder.");
+            //int userInput = Convert.ToInt32(Console.ReadLine());
+            //decimal total = userInput % 7;
+            //Console.WriteLine("Total is : " + total);
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
--- VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -11,22 +11,22 @@ namespace VariablesAndDataTypes
         static void Main(string[] args)
         {
             //// This is a simple program=========TO COMMET CTRL+K+C OR CTRL+K+U
-            //Console.WriteLine("What is your name?");
-            //string yourName = Console.ReadLine();
-            //Console.WriteLine("Your name is:" + yourName);
-            //Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Your name is:" + yourName);
+            Console.ReadLine();
 
-            //Console.WriteLine("What is your favorite number?");
-            //string favoriteNumber = Console.ReadLine();
-            //int favNum = Convert.ToInt32(favoriteNumber);
-            //int total = favNum + 5;
-            //Console.WriteLine("Your favorite number plus 5 is:" + total);
-            //Console.ReadLine();
+            Console.WriteLine("What is your favorite number?");
+            string favoriteNumber = Console.ReadLine();
+            int favNum = Convert.ToInt32(favoriteNumber);
+            int total = favNum + 5;
+            Console.WriteLine("Your favorite number plus 5 is:" + total);
+            Console.ReadLine();
 
-            //bool isStudying = false;
-            //byte hoursWorked = 42;
-            //sbyte currentTemperature = -23;
-            //char randomLetter = "X";
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char randomLetter = "X";
             char questionMark = '\u2103';
 
             decimal moneyInBank = 100.5m;
