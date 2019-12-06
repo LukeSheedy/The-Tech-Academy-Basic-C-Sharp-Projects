4b46916c75f4f2016a320a7f17cea0e7e1a626ed
Luke Sheedy <thesheedy@hotmail.com>
2019-12-05 17:56:20 -0800

    Cleaned up Var name and Comment
    Made it so you can run all and cleaned up the var names and comments

--- Exercise 66/Exercise 66/Program.cs
+++ Exercise 66/Exercise 66/Program.cs
@@ -9,38 +9,38 @@ namespace Exercise_66
 
             ////1:multiplies by 50 
             //Console.WriteLine("Provide a number  to multiply by 50 :");
-            //int userInput = Convert.ToInt32(Console.ReadLine());
-            //int mult = userInput * 50;
-            //Console.WriteLine("Reslut is " + mult);
-            //Console.ReadLine();
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int mult = userInput * 50;
+            Console.WriteLine("Reslut is " + mult);
+            Console.ReadLine();
 
             ////2: takes input of user adds 25
             //Console.WriteLine("Provide a number so we can add 25 :");
-            //int userInput = Convert.ToInt32(Console.ReadLine());
-            //int add = userInput + 25;
-            //Console.WriteLine("Reslut is " + add);
-            //Console.ReadLine();
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+            int add = userInput2 + 25;
+            Console.WriteLine("Reslut is " + add);
+            Console.ReadLine();
 
             ////3: takes inputer and devies by 12.5
-            //Console.WriteLine("Provide a number so we can divide by 12.5.");
-            //int userInput = Convert.ToInt32(Console.ReadLine());
-            //decimal divid = 12.5m;
-            //decimal total = userInput / divid;
-            //Console.WriteLine("Your total is : "+total);
-            //Console.ReadLine();
+            Console.WriteLine("Provide a number so we can divide by 12.5.");
+            int userInput3 = Convert.ToInt32(Console.ReadLine());
+            decimal divid = 12.5m;
+            decimal total = userInput3 / divid;
+            Console.WriteLine("Your total is : " + total);
+            Console.ReadLine();
 
             //4: takes input and checks if greater than 50 and prints true or false
-            //Console.WriteLine("Please provide a number :");
-            //int userInput = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine(userInput > 50); // WILL return True  if userInput is higher than 50 
-            //Console.ReadLine();
+            Console.WriteLine("Please provide a number :");
+            int userInput4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(userInput4 > 50); // WILL return True  if userInput is higher than 50 
+            Console.ReadLine();
 
             //5: takes input from user divides by 7 and returns remiander
-            //Console.WriteLine("Provide a number so we can divide by 7 and see the remainder.");
-            //int userInput = Convert.ToInt32(Console.ReadLine());
-            //decimal total = userInput % 7;
-            //Console.WriteLine("Total is : " + total);
-            //Console.ReadLine();
+            Console.WriteLine("Provide a number so we can divide by 7 and see the remainder.");
+            int userInput5 = Convert.ToInt32(Console.ReadLine());
+            decimal total2 = userInput5 % 7;
+            Console.WriteLine("Total is : " + total2);
+            Console.ReadLine();
 
 
 
