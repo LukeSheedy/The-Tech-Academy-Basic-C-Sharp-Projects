d8a75c7044cc83e9e490aab0379f8c89c26d2456
Luke Sheedy <thesheedy@hotmail.com>
2019-12-09 12:40:48 -0800

    Keep CODING!

---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Luke";
+            //string quote = "The man said, \\Hello\", Jesse. \n Hello on a new line. \n \t Hello on a tab.";
+            //string filename = @"C:\\Users\\Luke";///when see file names use double backslash
+            //////OR 
+            ////string filename = @"C:\Users\Luke";
+
+
+            //bool trueOrFalso = name.Contains("u");
+            //trueOrFalso = name.EndsWith("u");
+
+            //int length = name.Length;
+
+
+            ////name = name.ToUpper();
+            ////name = name.ToLower();
+
+            //Console.WriteLine(length);
+            //Console.ReadLine();
+
+
+            //string name = "Luke";
+            //name = "Joe";
+
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("my name is Jesse");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
