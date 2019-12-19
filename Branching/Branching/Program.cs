b30ada86dec21d31fe411b3e73a9fc0a4abb1604
Luke Sheedy <thesheedy@hotmail.com>
2019-12-19 12:36:12 -0800

    Coding!

--- Branching/Branching/Program.cs
+++ Branching/Branching/Program.cs
@@ -21,55 +21,55 @@ namespace Branching
 
 
 
-            //int roomTemperature = 70;
+            int roomTemperature = 70;
 
-            //Console.WriteLine("Hi, what is your name?");
-            //string name = Console.ReadLine();
+            Console.WriteLine("Hi, what is your name?");
+            string name = Console.ReadLine();
 
-            //Console.WriteLine("Hi," + name + ", what is the temperature where you are?");
-            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hi," + name + ", what is the temperature where you are?");
+            int currentTemperature = Convert.ToInt32(Console.ReadLine());
 
-            //if (currentTemperature == roomTemperature)
-            //{
-            //    Console.WriteLine("It is exactly room temperature.");
-            //}
-            //else if (currentTemperature > roomTemperature)
-            //{
-            //    Console.WriteLine("It is warmer than room temperature");
-            //}
-            //else if (roomTemperature > currentTemperature)
-            //{
-            //    Console.WriteLine("It is colder than room temperature");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("uhhhh somethings not right..");
-            //}
-            //Console.ReadLine();
+            if (currentTemperature == roomTemperature)
+            {
+                Console.WriteLine("It is exactly room temperature.");
+            }
+            else if (currentTemperature > roomTemperature)
+            {
+                Console.WriteLine("It is warmer than room temperature");
+            }
+            else if (roomTemperature > currentTemperature)
+            {
+                Console.WriteLine("It is colder than room temperature");
+            }
+            else
+            {
+                Console.WriteLine("uhhhh somethings not right..");
+            }
+            Console.ReadLine();
 
-            //int currentTemperature = 80;
-            //int troomTemperature = 70;
+            int currentTemperature = 80;
+            int troomTemperature = 70;
 
-            //string comparisonResult = currentTemperature == troomTemperature ? "It is room temp." : "It is not room temp.";
-            //Console.WriteLine(comparisonResult);
-            //Console.ReadLine();
+            string comparisonResult = currentTemperature == troomTemperature ? "It is room temp." : "It is not room temp.";
+            Console.WriteLine(comparisonResult);
+            Console.ReadLine();
 
-            //if (currentTemperature == troomTemperature)
-            //{
-            //    Console.WriteLine("It is exactly room temperature.");
-            //}
-            //else if (currentTemperature > troomTemperature)
-            //{
-            //    Console.WriteLine("It is warmer than room temperature.");
-            //}
-            //else if (troomTemperature > currentTemperature)
-            //{
-            //    Console.WriteLine("Room temperature is warmer than current temperature.");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("It is not exactly room temperature.");
-            //}
+            if (currentTemperature == troomTemperature)
+            {
+                Console.WriteLine("It is exactly room temperature.");
+            }
+            else if (currentTemperature > troomTemperature)
+            {
+                Console.WriteLine("It is warmer than room temperature.");
+            }
+            else if (troomTemperature > currentTemperature)
+            {
+                Console.WriteLine("Room temperature is warmer than current temperature.");
+            }
+            else
+            {
+                Console.WriteLine("It is not exactly room temperature.");
+            }
 
         }
     }
